f87eebc70f135888928e1b0fed3e0cb36dd63cae
Wilson Souto dos Reis <wilsonsdr@gmail.com>
2022-09-08 20:59:03 -0300

    calculadora de media e conversor de temperaturas

---
+++ aula-01/README.md
@@ -0,0 +1,11 @@
+![Screenshot from 2022-09-08 20-49-58](https://user-images.githubusercontent.com/81364355/189244914-2bc46f81-73c4-46fd-a4c7-b2875965fef9.png)
+
+# Descrição do projeto
+
+- Este é o projeto da aula 01 - Imersão Dev da Alura.
+
+- A ideia do projeto é desenvolver uma calculadora de média e um conversor de temperaturas.
+
+- O design da página desenvolvi com base no terminal do Mint, que serviu de inspiração.
+
+- Todo o foco do projeto é voltado para JavaScript.
---
+++ aula-01/script.js
@@ -0,0 +1,38 @@
+const informe = prompt("Informe seu nome:");
+let n1 = prompt("Informe sua nota N1");
+let n2 = prompt("Informe sua nota N2");
+let n3 = prompt("Informe sua nota N3");
+let n4 = prompt("Informe sua nota N4");
+let celsius = prompt("Informe a temperatura em Celsius");
+let fahrenheit = prompt("Informe a temperatura em Fahrenheit");
+const nome = document.querySelector(".nome");
+const conteudo = document.querySelector(".main-container");
+
+n1 = Number(n1);
+n2 = Number(n2);
+n3 = Number(n3);
+n4 = Number(n4);
+celsius = Number(celsius);
+fahrenheit = Number(fahrenheit);
+
+resultado = (n1 + n2 + n3 + n4) / 4; // soma as notas e divide por 4 para saber a média
+conversaoCF = celsius * 1.8 + 32;
+conversaoFC = (fahrenheit - 32) / 1.8;
+
+nome.innerHTML = `${informe}@imersao-dev-alura: ~`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">nota primeiro bimestre ${n1}</span></h3>`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">nota segundo bimestre ${n2}</span></h3>`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">nota terceiro bimestre ${n3}</span></h3>`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">nota quarto bimestre ${n4}</span></h3>`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">a sua media e de ${resultado}</span></h3>`;
+
+if (resultado >= 6) {
+  aprovado =
+    conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">voce foi aprovado</span></h3>`;
+} else {
+  reprovado =
+    conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">voce foi reprovado</span></h3>`;
+}
+
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">${celsius}ºC equivale a ${conversaoCF}ºF</span></h3>`;
+conteudo.innerHTML += `<h3>${informe}@imersao-dev-alura<span class="branco">:</span><span class="azul">~</span><span class="branco">$</span><span class="texto">${fahrenheit}ºF equivale a ${conversaoFC}ºC</span></h3>`;
